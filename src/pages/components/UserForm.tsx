8886d171527a1c81c5a9db0152dfb880b0c359a7
Georgesui <gdimov445@gmail.com>
2023-05-17 17:27:47 +0300

    new commit

---
+++ src/pages/components/UserForm.tsx
@@ -0,0 +1,39 @@
+import React, {FC, useState } from 'react';
+import Link from 'next/link';
+import { User} from '../services/user';
+import {SEARCH_USER_URL} from '../services/index'
+
+const UserForm:FC =  () => {
+
+	const [filteredData, setfilteredData] = useState<string>('');
+	const [userFiltered, setUserFlitered] = useState<User[]>([]);
+
+	const resultOfFormSearch = async (event: React.ChangeEvent<HTMLInputElement>) => {
+		const filtered = event.target.value;
+		setfilteredData(filtered)
+	
+	if(filtered) {
+		try {
+			const res = await fetch(`${SEARCH_USER_URL}${filteredData}`);
+			if(!res.ok) throw new Error('Error fetching filter')
+			const filtered = await res.json();
+			setUserFlitered(filtered.users)
+		}
+		catch (err) {throw new Error('Error fetching filter')}
+	} else {
+		setUserFlitered([])
+	}
+	}
+	return (
+		<ul className='userFormClass'>
+	<input placeholder='search' value={filteredData} onChange={resultOfFormSearch} className='inputForm'/>
+		{userFiltered.map((el)=> (<div key={el.id}>
+		<Link href={`/users/${el.id}`}>
+					<li className='findedInList'>{el.firstName}{' '}{el.lastName}</li>
+					</Link>
+	 </div>))}
+		</ul>
+	)
+}
+
+export default UserForm;
